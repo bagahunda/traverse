7b3c827b6655217df9db18d2eab2c6fec437e851
alex <alex.nm2012@gmail.com>
2016-10-06 15:05:59 +0300

    fixes for tablet

--- src/templates/blocks/platform/platform.js
+++ src/templates/blocks/platform/platform.js
@@ -16,14 +16,14 @@
     setEqualHeight('inner-container', 'feature__img');
 
 
-    $(window).on('resize', function () {
-      var winWidth = $(window).width();
-      if (winWidth === 499) {
-        $('.js-bx-slider').bxSlider({});
-      } else if (winWidth === 501) {
-        $('.js-bx-slider').destroySlider();
-      }
-    });
+    // $(window).on('resize', function () {
+    //   var winWidth = $(window).width();
+    //   if (winWidth === 499) {
+    //     $('.js-bx-slider').bxSlider({});
+    //   } else if (winWidth === 501) {
+    //     $('.js-bx-slider').destroySlider();
+    //   }
+    // });
 
   });
 
