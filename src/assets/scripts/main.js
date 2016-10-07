be9e42ef1db929d1a7fc3e34e36d585ca25c2b18
alex <alex.nm2012@gmail.com>
2016-10-07 13:19:39 +0300

    wip modal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,35 +1,34 @@
 (function ($) {
 
-  function arrayToRGB ( array ) {
-  return "rgb(" + array[0] + ", " + array[1] + ", " + array[2] + ")";
-  }
-
-  function processImages () {
-    var targetImages = $('.hero__gradient img')
-    targetImages.each(function (index) {
-      var targetImage = targetImages [index];
-      var targetImage1 = targetImages [index + 1];
-
-
-
-      var colorThief = new ColorThief();
-      var startColor = colorThief.getColor (targetImage);
-      if (targetImage1 === undefined) {
-        var endColor = [250, 250, 250];
-      } else {
-        var endColor = colorThief.getColor (targetImage1);
-      }
-      console.log($(this).parent());
-      $(this).parent().css({
-        'background': 'linear-gradient(to bottom, ' + arrayToRGB(startColor) + ', ' + arrayToRGB(endColor) + ')'
-      })
-
-    });
-  }
+  // function arrayToRGB ( array ) {
+  // return "rgb(" + array[0] + ", " + array[1] + ", " + array[2] + ")";
+  // }
+  //
+  // function processImages () {
+  //   var targetImages = $('.hero__gradient img')
+  //   targetImages.each(function (index) {
+  //     var targetImage = targetImages [index];
+  //     var targetImage1 = targetImages [index + 1];
+  //
+  //
+  //
+  //     var colorThief = new ColorThief();
+  //     var startColor = colorThief.getColor (targetImage);
+  //     if (targetImage1 === undefined) {
+  //       var endColor = [250, 250, 250];
+  //     } else {
+  //       var endColor = colorThief.getColor (targetImage1);
+  //     }
+  //     console.log($(this).parent());
+  //     $(this).parent().css({
+  //       'background': 'linear-gradient(to bottom, ' + arrayToRGB(startColor) + ', ' + arrayToRGB(endColor) + ')'
+  //     })
+  //
+  //   });
+  // }
 
   $(document).ready(function () {
 
-    processImages();
 
   });
 
