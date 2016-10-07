5abe3cbb8f6f71882a039bd210a8bbd648c82314
alex <alex.nm2012@gmail.com>
2016-10-07 17:31:19 +0300

    add hydro modal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,7 +7,6 @@
       if ($(this).hasClass('modal-enlarge')) {
         $(this).removeClass('modal-enlarge');
         $(this).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-          console.log($(window).width() - ($(this).width() + $(this).offset().left));
           $(this).parents('.bx-viewport').css({
             'overflow': 'hidden'
           })
@@ -18,12 +17,40 @@
           'overflow': 'visible'
         });
         $(this).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-          console.log($(window).width() - ($(this).width() + $(this).offset().left));
           $(this).parents('.bx-viewport').css({
             'overflow': 'visible'
           })
         });
       }
+    });
+
+    $('.js-show-modal').on('click', function (e) {
+
+      e.preventDefault();
+
+      var target = $(this).data('modal');
+      var winWithoutModal = $(window).width();
+
+      $('body').addClass('modal-shown');
+
+      var winWithModal = $(window).width();
+
+      $('#' + target).addClass('modal--visible');
+      $('body').css({
+        'margin-right': winWithModal - winWithoutModal
+      });
+
+    });
+
+    $('.js-close-modal').on('click', function (e) {
+
+      e.preventDefault();
+
+      $(this).parent('.modal').removeClass('modal--visible');
+      $('body').removeClass('modal-shown').css({
+        'margin-right': 0
+      });
+
     });
 
   });
