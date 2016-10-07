6658a24bb93c396d04d7dd3433cf6fc778499314
alex <alex.nm2012@gmail.com>
2016-10-07 15:54:03 +0300

    wip modal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,8 +1,30 @@
 (function ($) {
 
-  
+
   $(document).ready(function () {
 
+    $('.modal .flex img').on('click', function() {
+      if ($(this).hasClass('modal-enlarge')) {
+        $(this).removeClass('modal-enlarge');
+        $(this).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+          console.log($(window).width() - ($(this).width() + $(this).offset().left));
+          $(this).parents('.bx-viewport').css({
+            'overflow': 'hidden'
+          })
+        });
+      } else {
+        $(this).addClass('modal-enlarge');
+        $(this).parents('.bx-viewport').css({
+          'overflow': 'visible'
+        });
+        $(this).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+          console.log($(window).width() - ($(this).width() + $(this).offset().left));
+          $(this).parents('.bx-viewport').css({
+            'overflow': 'visible'
+          })
+        });
+      }
+    });
 
   });
 
